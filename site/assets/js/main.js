13c1b3da25fd7f09777c5fa20d6b70bc25ce0b93
Mark <mark_evillive@hotmail.com>
2021-03-04 19:17:24 +0700

    starting_template

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,3 +1,11 @@
+$(function(){
 
+    $(window).on('load', function(){
+        $('.preloader').delay(500).fadeOut('slow', function(){
+            $(this).attr('style', 'display: none !important');
+        });
+    });
+
+    
 
 
