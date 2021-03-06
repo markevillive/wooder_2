8bd0f972d2ceed1bbcd298fce652e4b0fa10f074
Mark <mark_evillive@hotmail.com>
2021-03-06 12:50:11 +0700

    add scrollup

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,13 +1,24 @@
-$(document).ready(function() {
-    $('.preloader').delay(500).fadeOut('slow', function() {
-        $(this).attr('style', 'display: none !important');
+$(function (){
+    $(window).on('load', function(){
+        $('.preloader').delay(500).fadeOut('slow', function(){
+            $(this).attr('style', 'display: none !important');
+        });
     });
+
+
+    $(window).scroll(function( ){
+        if ($(this).scrollTop() > 200) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    $('.scrollToTop').click(function() {
+        $('html, body').animate({scrollTop : 0}, 800);
+        return false;
+
+    });
+
 });
-
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
-
-
-
 
