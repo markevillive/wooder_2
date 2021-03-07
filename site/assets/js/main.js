60e20f5b728c437b31fd4a24dcfc3fdbd75726f4
Mark <mark_evillive@hotmail.com>
2021-03-07 09:57:35 +0700

    owl-carousel

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -19,6 +19,30 @@ $(function (){
         return false;
 
     });
+    // Owl Carousel
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel();
+    });
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        merge: true,
+        loop: true,
+        margin: 10,
+        video: true,
+        lazyLoad: true,
+        center: true,
+        responsive: {
+            320: {
+                items: 1
+            },
+            560: {
+                items: 2
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
 
 });
 
